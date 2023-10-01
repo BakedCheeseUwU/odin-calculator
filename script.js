2fa1561ed6f99a66290c162aec2b5d4a11dd9f07
Aditya <dev.adityasus@gmail.com>
2023-10-02 01:18:03 +0530

    Implements compute function

--- script.js
+++ script.js
@@ -28,13 +28,41 @@ class Calculator {
     this.currentOperand = "";
   }
 
-  compute() {}
+  compute() {
+    let result;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operation) {
+      case "+":
+        result = prev + current;
+        break;
+      case "-":
+        result = prev - current;
+        break;
+      case "*":
+        result = prev * current;
+        break;
+      case "/":
+        result = prev / current;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = result;
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
     this.previousOperandTextElement.innerText = this.previousOperand;
   }
 }
+
+// Selecting all the buttons
 const numberBtns = document.querySelectorAll("[data-number]");
 const operationBtns = document.querySelectorAll("[data-operation]");
 const equalBtn = document.querySelector("[data-equals]");
@@ -47,11 +75,13 @@ const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
 
+// Creating a new calculator object
 const calculator = new Calculator(
   previousOperandTextElement,
   currentOperandTextElement
 );
 
+// Adds numbers to the screen when clicked
 numberBtns.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
@@ -59,9 +89,16 @@ numberBtns.forEach((button) => {
   });
 });
 
+// Selects the operator
 operationBtns.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
+});
+
+// Computes the result when '=' is pressed
+equalBtn.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
 });
